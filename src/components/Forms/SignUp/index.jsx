7cacb3c0afd07504a29487c7458c6ddcd113f531
Jorge Castillo <jorgecastillo@waydda.com>
2019-12-12 16:25:59 -0600

    Form

--- src/components/Forms/SignUp/index.jsx
+++ src/components/Forms/SignUp/index.jsx
@@ -19,14 +19,16 @@ const SignUpForms = () => {
   };
 
   return (
-    <form className="row u__no_margin">
+    <form className={`row u__no_margin with_elevation ${styles.form_container}`}>
       <div className="col-12 u__no_padding">
+        <h1>{es.components.forms.signUp.title}</h1>
+        <p style={{ paddingBottom: '1.5em' }}>{es.components.forms.signUp.message}</p>
         <TextField
-          label={es.fields.name}
+          label={es.fields.name.label}
           required
           name="name"
           autoComplete="on"
-          placeholder="Richard Hendricks"
+          placeholder={es.fields.name.placeholder}
           spellCheck
           onChange={handleChange}
           minLength={3}
@@ -39,12 +41,12 @@ const SignUpForms = () => {
           }}
         />
         <TextField
-          label={es.fields.username}
+          label={es.fields.username.label}
           maxLength={15}
           minLength={1}
           required
           value={fields.username}
-          placeholder="richardhen"
+          placeholder={es.fields.username.placeholder}
           name="username"
           onChange={handleChange}
           style={{
@@ -56,12 +58,12 @@ const SignUpForms = () => {
         <TextField
           autoComplete="on"
           inputMode="email"
-          label={es.fields.email}
+          label={es.fields.email.label}
           maxLength={40}
           minLength={10}
           name="email"
           onChange={handleChange}
-          placeholder="richard@piedpiper.com"
+          placeholder={es.fields.email.placeholder}
           required
           type="email"
           value={fields.email}
@@ -72,16 +74,21 @@ const SignUpForms = () => {
           }}
         />
         <TextField
-          label={es.fields.password}
+          label={es.fields.password.label}
           maxLength={15}
           minLength={6}
           name="password"
           onChange={handleChange}
-          placeholder="******"
+          placeholder={es.fields.password.placeholder}
           value={fields.password}
           required
           type="password"
         />
+        <div className="row justify-content-end u__no_margin" style={{ paddingTop: '1.5em' }}>
+          <button type="button" className="with_elevation">
+            {es.components.forms.signUp.button}
+          </button>
+        </div>
       </div>
     </form>
   );
--- src/virtual_pages/Index/Unauthenticated/index.jsx
+++ src/virtual_pages/Index/Unauthenticated/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Form from '../../../components/Forms/SignUp';
 
 const UnauthenticatedIndex = () => (
-  <div className="row u__no_margin justify-content-center">
+  <div className="row u__no_margin justify-content-center full_height align-items-center">
     <div className="col-11 u__no_padding">
-      <div className="row u__no_margin">
-        <div className="col-5 u__no_padding">
+      <div className="row justify-content-center u__no_margin">
+        <div className="col-12 col-sm-8 col-md-6 col-lg-4 u__no_padding">
           <Form />
         </div>
       </div>
